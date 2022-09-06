4136b4373c4b002c7fe920890738bed2fa035a2c
Chael Placido <79405891+Placidoo@users.noreply.github.com>
2022-09-06 23:02:53 +0800

    Add files via upload

---
+++ applicationBiodata_Meraña/applicationBiodata_Meraña/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: joedm
+ * Date: 06/09/2022
+ * Time: 9:21 am
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace applicationBiodata_Meraña
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(47, 45);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(336, 27);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "What do you want to know about Me?";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(57, 87);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(306, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Name";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(57, 137);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(306, 44);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Gender";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(57, 187);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(306, 44);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Age";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(57, 237);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(306, 44);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Address";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(57, 287);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(306, 44);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "Course, Year and Section";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(421, 356);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Meraña";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ applicationBiodata_Meraña/applicationBiodata_Meraña/MainForm.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace applicationBiodata_Meraña
+{
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("My name is Joed Michael C. Meraña");
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("I am a Male.");
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("I am 19 years old.");
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("I live in Pangil Laguna, Philippines.");
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("I am currently taking Bachelor of Science in Computer Science 1A.");
+		}
+	}
+}
